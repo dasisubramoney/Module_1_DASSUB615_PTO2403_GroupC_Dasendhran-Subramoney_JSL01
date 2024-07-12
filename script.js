0cc6b916409466bfcbf374a6c9d169393fa9b93b
Dasendhran Subramoney <das.subramoney@gmail.com>
2024-07-12 12:02:08 +0200

    Added Functionality

--- script.js
+++ script.js
@@ -1,12 +1,32 @@
 function validateSyntax() {
     let input = document.getElementById('petInput').value;
-    // Validation logic goes here
+    let Petval = input.substr(0,4) // Stores first 4 characters of the string
+    let Userval= input.substr(5) // Stores rest of the string
     let result = ''; // Placeholder for validation result
+    var circle = document.getElementById('circle');
 
-    // TODO: Write your validation logic here
-        // Check if input starts with 'pet_' and followed by alphanumeric characters
+    //Function to check whether the string contains a letters and numbers 
+    function onlyLettersAndNumbers(Str) {
+        return /^[A-Za-z0-9]*$/.test(Str);
+    }
 
-            document.getElementById('result').innerText = result;
+    // Validation logic to check whether the strings contains pet_ and has letters and numbers following 
+    if (Petval == 'pet_' && onlyLettersAndNumbers(Userval) == true ) {
+        result = 'Valid Syntax'
+        document.getElementById('result').innerText = result;
+    
+        circle.style.backgroundColor = "green";
+        
+    } else {
+        result = 'Invalid Syntax'
+        document.getElementById('result').innerText = result;
+        
+        circle.style.backgroundColor = "red";
+    }
+
+    circle.style.visibility = 'visible';
+
+
 }
 
 
